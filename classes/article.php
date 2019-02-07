43690497947d998355ecb2395dd6af2b8cefcf20
ziadmk12 <ziyadmoukdad@gmail.com>
2019-02-07 18:23:09 +0100

    done

--- classes/article.php
+++ classes/article.php
@@ -1,120 +1,121 @@
 <?php
 class article{
 
-Private $Article_ID;
-private $Article_Name;
-private $CodeArticle;
-private $Reference;
-private $Stock;
-private $Unite;
-private $Reserve;
-private $Seuil;
-private $DateAddArt;
+Private $article_ID;
+private $article_Name;
+private $codeArticle;
+private $reference;
+private $stock;
+private $unite;
+private $reserve;
+private $seuil;
+private $dateAddArt;
 private $forni_id;
 private $id_cat;
 
-public function __construct($article_Name,$codeArticle,$reference,$stock,$unite,$Reserve,$seuil,$dateAddArt,$Forni_id,$Id_cat)
+public function __construct($article_Name,$codeArticle,$reference,$stock,$unite,$Reserve,$seuil,$dateAddArt,$forni_id,$id_cat)
 {
     
-    $this->Article_Name=$article_Name;
-    $this->CodeArticle=$codeArticle;
-    $this->Reference=$reference;
-    $this->Stock=$stock;
-    $this->Reserve=$Reserve;
-    $this->Seuil=$seuil;
-    $this->DateAddArt=$dateAddArt;
-    $this->forni_id=$Forni_id;
-    $this->id_cat=$Id_cat;
+    $this->article_Name=$article_Name;
+    $this->codeArticle=$codeArticle;
+    $this->reference=$reference;
+    $this->stock=$stock;
+    $this->unite=$unite;
+    $this->reserve=$reserve;
+    $this->seuil=$seuil;
+    $this->dateAddArt=$dateAddArt;
+    $this->forni_id=$forni_id;
+    $this->id_cat=$id_cat;
 }
 ////////////////////
 public function getArticle_ID()
 {
-    return $this->Article_ID;
+    return $this->article_ID;
 }
 public function setArticle_ID($article_ID)
 {
-    $this->Article_ID=$article_ID;
+    $this->article_ID=$article_ID;
 }
 ////////////////////
 public function getArticle_Name()
 {
-    return $this->Article_Name;
+    return $this->article_Name;
 }
 public function setArticle_Name($article_Name)
 {
-    $this->Article_Name=$article_Name;
+    $this->article_Name=$article_Name;
 }
 ////////////////////
 public function getCodeArticle()
 {
-    return $this->CodeArticle;
+    return $this->codeArticle;
 }
 public function setCodeArticle($codeArticle)
 {
-    $this->CodeArticle=$codeArticle;
+    $this->codeArticle=$codeArticle;
 }
 ////////////////////
 public function getReference()
 {
-    return $this->Reference;
+    return $this->reference;
 }
 public function setReference($reference)
 {
-    $this->Reference=$reference;
+    $this->reference=$reference;
 }
 ////////////////////
 public function getStock()
 {
-    return $this->Stock;
+    return $this->stock;
 }
 public function setStock($stock)
 {
-    $this->Stock=$stock;
+    $this->stock=$stock;
 }
 ////////////////////
 public function getReserve()
 {
-    return $this->Reserve;
+    return $this->reserve;
 }
 public function setReserve($reserve)
 {
-    $this->Reserve=$reserve;
+    $this->reserve=$reserve;
 }
 ////////////////////
 public function getSeuil()
 {
-    return $this->Seuil;
+    return $this->seuil;
 }
 public function setSeuil($seuil)
 {
-    $this->Seuil=$seuil;
+    $this->seuil=$seuil;
 }
 ////////////////////
 public function getDateAddArt()
 {
-    return $this->DateAddArt;
+    return $this->dateAddArt;
 }
 public function setDateAddArt($dateAddArt)
 {
-    $this->DateAddArt=$dateAddArt;
+    $this->dateAddArt=$dateAddArt;
 }
 ////////////////////
 public function getforni_id()
 {
     return $this->forni_id;
 }
-public function setforni_id($Forni_id)
+public function setforni_id($forni_id)
 {
-    $this->forni_id=$Forni_id;
+    $this->forni_id=$forni_id;
 }
 ////////////////////
 public function getid_cat()
 {
     return $this->id_cat;
 }
-public function setid_cat($Id_cat)
+public function setid_cat($id_cat)
 {
-    $this->id_cat=$Id_cat;
+    $this->id_cat=$id_cat;
 }
 }
 
