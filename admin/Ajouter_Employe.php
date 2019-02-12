6318f0ec9876ab814bac50c8f9e0afd8e7ffc7ce
Banahoks Winners <banahoks.wydad@gmail.com>
2019-02-12 19:40:00 +0100

    update interface ajouter employee

--- admin/Ajouter_Employe.php
+++ admin/Ajouter_Employe.php
@@ -1,8 +1,6 @@
 <?php
 include_once 'include/header.php';
 ?>
-
-
     <!-- Main content -->
     <section class="content">
       <div class="row">
@@ -17,21 +15,74 @@ include_once 'include/header.php';
             <!-- form start -->
             <form role="form">
               <div class="box-body">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+              <div class="row">
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Matricule</label>
+                  <input name="matricule" id="matricule" type="text" class="form-control"  >
                 </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Nom</label>
+                  <input name="nom" id="nom" type="text" class="form-control" >
                 </div>
-               
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Prenom</label>
+                  <input name="Prenom" id="Prenom" type="text" class="form-control" >
+                </div>
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Date Naissance</label>
+                  <input name="dateN" id="dateN" type="date" class="form-control"  >
+                </div>
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Adresse</label>
+                  <input name="adresse" id="adresse" type="text" class="form-control"  >
+                </div>
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input name="email" id="email" type="text" class="form-control"  >
+                </div>
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Telephone</label>
+                  <input name="tele" id="tele" type="text" class="form-control"  >
+                </div>
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Image</label>
+                  <input name="image" id="image" type="file" class="form-control"  >
+                </div>
+              </div>
+              <div class="col-md-4">
+              <div class="form-group">
+                  <label for="exampleInputEmail1">Grade</label>
+                  <select class="form-control" name="grade" id="grade">
+                  <option>admin</option>
+                  <option>employee</option>
+                  </select>
+                </div>
+              </div>
+             
+             
+              </div>
+              </div>
                 
               </div>
               <!-- /.box-body -->
 
               <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" id="ajouter" class="btn btn-primary">Ajouter</button>
               </div>
             </form>
           </div>
