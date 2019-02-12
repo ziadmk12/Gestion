c417919ed539ce1dc53bc1223042d44a932be56f
ziadmk12 <ziyadmoukdad@gmail.com>
2019-02-13 00:47:11 +0100

    complet Ajout Emp ziad

--- admin/api/add_employe.php
+++ admin/api/add_employe.php
@@ -9,9 +9,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $matricule=$_POST['matricule'];
     $nom=$_POST['nom'];
     $prenom=$_POST['prenom'];
-    
+    $datenaissance=$_POST['dateN'];
+    $adresse=$_POST['adresse'];
+    $email=$_POST['email'];
+    $telefone=$_POST['tele'];
+    $image=$_POST['image'];
+    $grade=$_POST['grade'];
 
     header('Content-type: application/json');
 
-    echo json_encode($matricule."/".$nom."/".$prenom);
+    echo json_encode($matricule."/".$nom."/".$prenom."/".$datenaissance."/".$adresse."/".$email."/".$telefone."/".$image."/".$grade);
 }
